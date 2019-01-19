b18868bd97fb97f81988435fd3e70ba7bf0432b3
Jamal Gelin <gelin.jamal@gmail.com>
2019-01-19 05:06:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+cclass Student < User
 
+   def initialize
+    @knowledge = []
+  end
+
+   def learn(item_learned)
+    @knowledge << item_learned
+  end
+
+   def knowledge
+    @knowledge
+  end
 end
